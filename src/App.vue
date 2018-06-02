976b5f4f3e9b0d2c8c0145a26be9b27e405e0e8d
Natthaphon Phanit <nulladivides@protonmail.com>
2018-06-03 00:09:11 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-servers></app-servers>
+            <app-server-detail></app-server-detail>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
